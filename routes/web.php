758d6ee5a9d27a3c3ee4cb9ffe7f6bb25dff9d8f
Rasyid Kusnady <rasid.rasid9961@gmail.com>
2023-09-26 14:46:20 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,11 @@ Route::get('/', function () {
     return view('welcome');
     
 });
-
-//route resource
-// Route::resource('/post', \App\Http\Controllers\PostController::class);
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
